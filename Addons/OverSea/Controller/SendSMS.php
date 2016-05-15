6d7849b4458f6932912469a325b3ea67b60d626c
Gao <jianguog@a0999b04f99b.ant.amazon.com>
2016-05-15 18:28:57 +0800

    ajax waiting sms

--- Addons/OverSea/Controller/SendSMS.php
+++ Addons/OverSea/Controller/SendSMS.php
@@ -9,9 +9,15 @@ require dirname(__FILE__).'/../init.php';
 use Addons\OverSea\Common\YunpianSMSHelper;
 
 header("Content-Type:text/html;charset=utf-8");
-$text="【云片网】您的验证码是1234";
+//$text="【云片网】您的验证码是1234";
 //$mobile = '+8613520143483';
+$text="【易知海外】您的验证码是1234";
 $mobile = '+14257362292';
-YunpianSMSHelper::sendSMS($text, $mobile);
+
+//YunpianSMSHelper::sendSMS($text, $mobile);
+
+session_start();
+echo json_encode(array('status'=>$_POST['code']));
+exit;
 
 ?>
